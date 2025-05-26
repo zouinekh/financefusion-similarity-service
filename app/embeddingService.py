9a782a5d09a85a46418e038a14a4e9921f38ed7b
zouinekh amine <zouinekhamine@gmail.com>
2025-05-26 16:51:36 +0000

    change similarity model

--- app/embeddingService.py
+++ app/embeddingService.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class EmbeddingService:
     def __init__(self):
-        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        self.model = SentenceTransformer('all-mpnet-base-v2')
 
     def embed_text(self, text: str) -> np.ndarray:
         return self.model.encode([text])[0]
